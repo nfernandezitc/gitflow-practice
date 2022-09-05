f784ef2dab492485b7a4c7984fe8d003967acc9c
Nicolas Fernandez <nfernandez@itcrowdarg.com>
2022-09-05 20:16:22 -0300

    Mensaje 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cambiando el mensaje por primera vez.
         </p>
         <a
           className="App-link"
